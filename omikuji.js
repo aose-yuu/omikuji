84c4783386328279840895738194935b4d430154
aose-yuu <aose.developer@gmail.com>
2021-10-06 03:26:48 +0900

    [Add]if文で条件分岐を作成

--- omikuji.js
+++ omikuji.js
@@ -5,3 +5,19 @@ userName = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = userName;
 
 let randNum = Math.floor(Math.random() * 5);
+
+if (randNum == 0) {
+    userResult = "大吉";
+}
+if (randNum == 1) {
+    userResult = "中吉";
+}
+if (randNum == 2) {
+    userResult = "小吉";
+}
+if (randNum == 3) {
+    userResult = "吉";
+}
+if (randNum == 4) {
+    userResult = "凶";
+}
